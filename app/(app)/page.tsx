f8292872a298b39e5f3bf8d5eb9b21dea9cb4a12
omrilandman <omriland@gmail.com>
2026-05-28 10:03:28 +0300

    לטעי

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,7 +1,6 @@
-import { DayView } from "@/components/meals/day-view";
+import { redirect } from "next/navigation";
 import { getJerusalemDateString } from "@/lib/dates/jerusalem";
 
 export default function TodayPage() {
-  const today = getJerusalemDateString();
-  return <DayView date={today} />;
+  redirect(`/day/${getJerusalemDateString()}`);
 }
